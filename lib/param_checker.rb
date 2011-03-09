238771173a6c828857c49c707a968b60529f0aec
Kai Schlamp <schlamp@gmx.de>
2011-03-09 12:35:29 +0100

    Added code documentation.

--- lib/param_checker.rb
+++ lib/param_checker.rb
@@ -2,6 +2,11 @@ module ParamChecker
 
   module_function
 
+  # Check a parameter string if it is a valid integer and return its integer value.
+  # +param+: the string parameter to check
+  # +default+: the default integer to return if the check fails
+  # +min+: the minimum value allowed (optional)
+  # +max+: the maximum value allowed (optional)
   def check_integer_param(param, default, min = nil, max = nil)
     min_lambda = (min.nil? ? lambda { true } : lambda { param.strip.to_i >= min })
     max_lambda = (max.nil? ? lambda { true } : lambda { param.strip.to_i <= max })
@@ -13,6 +18,11 @@ module ParamChecker
     end
   end
 
+  # Check a parameter string if it is a valid float and return its float value.
+  # +param+: the string parameter to check
+  # +default+: the default float to return if the check fails
+  # +min+: the minimum value allowed (optional)
+  # +max+: the maximum value allowed (optional)
   def check_float_param(param, default, min = nil, max = nil)
     min_lambda = (min.nil? ? lambda { true } : lambda { param.strip.to_i >= min })
     max_lambda = (max.nil? ? lambda { true } : lambda { param.strip.to_i <= max })
@@ -24,6 +34,11 @@ module ParamChecker
     end
   end
 
+  # Check a parameter string if it is a valid sting and return its string value.
+  # +param+: the string parameter to check
+  # +default+: the default string to return if the check fails
+  # +allowed+: the allowed string value to check +param+ against; could be
+  #   a regular expression, a string or an array of strings
   def check_string_param(param, default, allowed)
     if (param && allowed.class == Regexp && param =~ allowed)
       param
@@ -36,6 +51,11 @@ module ParamChecker
     end
   end
 
+  # Check a parameter string if it is a valid :symbol and return its symbol value.
+  # +param+: the string parameter to check
+  # +default+: the default symbol to return if the check fails
+  # +allowed+: the allowed symbol value to check +param+ against; could be
+  #   a regular expression, a string, a symbol, an array of strings or an array of symbols.
   def check_symbol_param(param, default, allowed)
     begin
       if (param && allowed.class == Regexp && param.to_s =~ allowed)
@@ -52,6 +72,10 @@ module ParamChecker
     end
   end
 
+  # Check a parameter string if it represents a valid boolean and return its boolean value.
+  # Allowed string parameters are "1" or "true" for +true+, and "0" or "false" for +false+.
+  # +param+: the string parameter to check
+  # +default+: the default boolean to return if the check fails
   def check_boolean_param(param, default)
     if (param && param == "1" || param == "true")
       true
